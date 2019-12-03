7a704484643e65f0e90e95df140c26364463d0b4
Ryan Pettingill <pettingillryan@gmail.com>
2019-12-02 17:46:31 -0700

    deployment mode

--- server.js
+++ server.js
@@ -61,9 +61,9 @@ app.use((err, req, res, next) => {
     return res.send({errMsg: err.message})
 })
 // added this line below for deployment
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html") )
-// })
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html") )
+})
 
 app.listen(PORT, () => {
     console.log(`[+] Server is running on PORT ${PORT}`)
